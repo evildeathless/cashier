d05c9545710d35024ef8439857e9800729994924
Bladimir Huaraya <bladimir.huaraya@gmail.com>
2021-02-23 00:30:11 -0500

    I have added product models

--- cajero/models.py
+++ cajero/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=12)
+    nombre = models.CharField(max_length=15)
+    unidad_medida = models.CharField(max_length=10)
+    precio = models.DecimalField(null=False, validators=[MinValueValidator(Decimal('0.01'))], max_digits=7, decimal_places=2)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to="productos", null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
