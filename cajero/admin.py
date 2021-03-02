36d84e6fd5371be82bc5b7675d2f5f444ec39a2d
Bladimir Huaraya <bladimir.huaraya@gmail.com>
2021-03-02 11:29:45 -0500

    Revert "I added sales model"
    This reverts commit 892a7ab67512ba0e9e8eb45f27043b5cc3ca1f6d.

--- cajero/admin.py
+++ cajero/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Producto, Venta
+from .models import Producto
 
 # Register your models here.
 admin.site.register(Producto)
-admin.site.register(Venta)
--- cajero/urls.py
+++ cajero/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home, login, productos, ventas
+from .views import home, login, productos
 
 urlpatterns = [
     path('', home, name="home"),
     path('login/', login, name="login"),
-    path('productos/', productos, name="productos"),
-    path('ventas/', ventas, name="ventas")
+    path('productos/', productos, name="productos")
 ]
