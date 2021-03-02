70df420eaf352115ed8bac527cd5f916421f72be
Bladimir Huaraya <bladimir.huaraya@gmail.com>
2021-03-02 15:48:23 -0500

    I added sales, cash register models

--- cajero/admin.py
+++ cajero/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Venta, Arqueo
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["codigo", "nombre", "unidad_medida", "precio", "created"]
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ["id", "id_user", "boleta", "total", "fecha"]
+    list_filter = ["fecha"]
+    date_hierarchy = "fecha"
+
+class ArqueoAdmin(admin.ModelAdmin):
+    list_display = ["id", "fecha", "total"]
 
 # Register your models here.
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Venta, VentaAdmin)
+admin.site.register(Arqueo, ArqueoAdmin)
--- cajero/models.py
+++ cajero/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 from decimal import Decimal
+from django.conf import settings
 
 # Create your models here.
 class Producto(models.Model):
@@ -15,3 +16,19 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Venta(models.Model):
+    id_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    total = models.DecimalField(null=False, validators=[MinValueValidator(Decimal('0.01'))], max_digits=7, decimal_places=2)
+    boleta = models.CharField(max_length=8)
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.id)
+
+class Arqueo(models.Model):
+    fecha = models.DateField(auto_now_add=True)
+    total = models.DecimalField(null=False, validators=[MinValueValidator(Decimal('0.01'))], max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.id)
